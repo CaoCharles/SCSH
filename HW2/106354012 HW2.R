37fb7428cf888d212e157dff78c0e2f298c236ea
CaoCharles <eric830123@gmail.com>
2018-05-21 18:10:27 +0800

    0521

--- HW2/106354012 HW2.R
+++ HW2/106354012 HW2.R
@@ -62,14 +62,15 @@ r1 <- rmvnorm(73984,mean=theta$mu1,sigma=theta$sigma1)
 colnames(r1) <- c("D1x","D1y")
 r2 <- rmvnorm(73984,mean=theta$mu2,sigma=theta$sigma2)
 colnames(r2) <- c("D2x","D2y")
-data <- cbind(data1,r1,r2)
+r <-rbind(r1,r2) 
+data1 <- rbind(data1,data1)
+data <- cbind(data1,r)
 A <- ggplot(data)+
       labs(title = " Waiting time vs Eruption time of old faithful geyser", x = "Eruptions time (min)", y = "Time to next eruption (min)")+
       xlim(0,6)+ ylim(40,100)+
       geom_point(aes(x = eruptions, y = waiting))+
       stat_density2d(aes(x = D1x,y = D1y),col = "red")+
       theme(title = element_text(face="bold"))+
-      stat_density2d(aes(x = D2x,y = D2y))+
       scale_fill_gradientn(colours=rainbow(9))+
       theme(panel.background = element_rect(colour="black",size = 2))
 return(A)
