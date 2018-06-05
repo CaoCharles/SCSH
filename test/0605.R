feee3c739d6ba11623609564a39f390747cdafe3
CaoCharles <eric830123@gmail.com>
2018-06-05 11:06:28 +0800

    0605

--- test/0605.R
+++ test/0605.R
@@ -1,6 +1,6 @@
 # 小考
 set.seed(2018)
-x_old = 0
+x_old = 5
 x = c()
 x_new = 0
 y=0
@@ -21,9 +21,22 @@ k=1*((x[-1]-x[-5000])!=0)
 acc_rate = sum(k)/5000
 acc_rate
 
+# (2)
 x <- as.matrix(x)
 plot.ts(t(x))
 mean(t(x));var(t(x))
 
-Y <- x[3001:5000]
+# (3)
+Y <- x[2001:5000]
 mean(Y);var(Y)
+
+# (4)
+library(dplyr)
+options(digits = 4)
+p <- NULL
+for (i in 0:10) {
+  px <- 1*(x[2001:5000]==i) %>% sum()
+  px <- px/3000
+  p <- c(p, px)
+}
+p
